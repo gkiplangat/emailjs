31adfb2a7db36e8364d244154df7bc1f1dd6eef7
Njepkemboi <nahum.jepkemboi@mpesafoundationacademy.ac.ke>
2024-09-25 21:55:37 +0300

    initial

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    const params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+    };
+
+    emailjs.send("service_4e1zl5j", "template_4s4fqra", params)
+        .then(function(response) {
+            alert("Email Sent Successfully");
+            console.log('SUCCESS!', response.status, response.text);
+            document.getElementById('contact-form').reset(); // Reset the form
+        }, function(error) {
+            alert("Failed to send email. Please try again later.");
+            console.log('FAILED...', error);
+        });
+});
